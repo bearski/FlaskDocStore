10cbc7a99b5beceb4751eee8d1811e76e2d67fec
bearski <santosh.sewlal@gmail.com>
2016-04-24 10:42:28 +0200

    config update for heroku mailgun and postgres

--- project/config.py
+++ project/config.py
@@ -15,19 +15,31 @@ class BaseConfig(object):
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
     # mail settings
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
+    # MAIL_SERVER = 'smtp.gmail.com'
+    # MAIL_PORT = 465
+    # MAIL_USE_TLS = False
+    # MAIL_USE_SSL = True
+
+    MAIL_SERVER = os.environ['smtp.mailgun.org']
+    MAIL_PORT = 587
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
+
     # gmail authentication
-    MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
-    MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+    # MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+    # MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+
+    MAIL_USERNAME = os.environ['MAILGUN_SMTP_LOGIN']
+    MAIL_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+
 
     # mail accounts
-    MAIL_DEFAULT_SENDER = os.environ['APP_MAIL_DEFAULT_SENDER']
+    # MAIL_DEFAULT_SENDER = os.environ['APP_MAIL_DEFAULT_SENDER']
+    MAIL_DEFAULT_SENDER = os.environ['MAILGUN_SMTP_LOGIN']
 
-    SQLALCHEMY_DATABASE_URI = os.environ['APP_SQLALCHEMY_DATABASE_URI']
+    #SQLALCHEMY_DATABASE_URI = os.environ['APP_SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 #class DevelopmentConfig(BaseConfig):
 #    """Development configuration."""
