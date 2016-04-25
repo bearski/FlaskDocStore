ff4a7fcc92b9ab89ea595fba944085cc593809f2
bearski <santosh.sewlal@gmail.com>
2016-04-25 08:30:25 +0200

    testing mailgun setup

--- project/config.py
+++ project/config.py
@@ -17,47 +17,37 @@ class BaseConfig(object):
     # mail settings
     # MAIL_SERVER = 'smtp.gmail.com'
     # MAIL_PORT = 465
-    # MAIL_USE_TLS = False
-    # MAIL_USE_SSL = True
 
-    MAIL_SERVER = os.environ['MAILGUN_SMTP_SERVER']
-    MAIL_PORT = 587
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+
+    MAIL_SERVER = 'smtp.mailgun.org' # os.environ['MAILGUN_SMTP_SERVER']
+    MAIL_PORT = 587 # os.environ['MAILGUN_SMTP_PORT']
+    MAIL_USE_TLS = True
 
 
     # gmail authentication
     # MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
     # MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
 
-    MAIL_USERNAME = os.environ['MAILGUN_SMTP_LOGIN']
-    MAIL_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
-    MAILGUN_KEY = os.environ['MAILGUN_KEY']
-    MAILGUN_KEY = os.environ['MAILGUN_DOMAIN']
+    MAIL_USERNAME = 'postmaster@appbffbce930f264e479c9e27fd294518d6.mailgun.org' # os.environ['MAILGUN_SMTP_LOGIN']
+    MAIL_PASSWORD = '1706b4fab5522717b7f68014bb7b39d9' # os.environ['MAILGUN_SMTP_PASSWORD']
+
+    # MAILGUN_API_KEY = os.environ['MAILGUN_API_KEY']
+    # MAILGUN_DOMAIN = os.environ['MAILGUN_DOMAIN']
+    # MAILGUN_PUBLIC_KEY = os.environ['MAILGUN_PUBLIC_KEY']
 
     # mail accounts
     # MAIL_DEFAULT_SENDER = os.environ['APP_MAIL_DEFAULT_SENDER']
-    MAIL_DEFAULT_SENDER = os.environ['MAILGUN_SMTP_LOGIN']
+    MAIL_DEFAULT_SENDER =  'postmaster@appbffbce930f264e479c9e27fd294518d6.mailgun.org' # os.environ['MAILGUN_SMTP_LOGIN']
 
     #SQLALCHEMY_DATABASE_URI = os.environ['APP_SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-#class DevelopmentConfig(BaseConfig):
-#    """Development configuration."""
-#    DEBUG = True
-#    WTF_CSRF_ENABLED = False
-#    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
-#    DEBUG_TB_ENABLED = True
-#
-#
 class TestingConfig(BaseConfig):
     """Testing configuration."""
     TESTING = True
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 1
     WTF_CSRF_ENABLED = False
-#    SQLALCHEMY_DATABASE_URI = 'sqlite://'
-
 
 class ProductionConfig(BaseConfig):
     """Production configuration."""
